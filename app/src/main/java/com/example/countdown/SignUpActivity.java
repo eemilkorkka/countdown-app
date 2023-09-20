ea91f3659526e874f6b21d8101f5bc8b74f296a4
= <eemil.korkka@koudata.fi>
2023-09-20 15:31:49 +0300

    User registration and login implemented

---
+++ app/src/main/java/com/example/countdown/SignUpActivity.java
@@ -0,0 +1,94 @@
+package com.example.countdown;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText emailEditText, passwordEditText, confirmPasswordEditText;
+    private Button signUpButton;
+    private FirebaseAuth firebaseAuth;
+    private String TAG = "SignUpActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        confirmPasswordEditText = findViewById(R.id.confirmPasswordEditText);
+        signUpButton = findViewById(R.id.signUpBtn);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                String confirmPassword = confirmPasswordEditText.getText().toString();
+
+                if (email.isEmpty()) {
+                    emailEditText.setError("You must enter an email");
+                }
+
+                if (password.isEmpty() || confirmPassword.isEmpty()) {
+                    passwordEditText.setError("You must enter a password");
+                }
+
+                if (password.equals(confirmPassword)) {
+                    createAccount(email, password);
+                } else {
+                    passwordEditText.setError("Passwords do not match!");
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        if (currentUser != null) {
+            reload();
+        }
+    }
+
+    private void createAccount(String email, String password) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in was successful, display a message to the user.
+                            Toast.makeText(SignUpActivity.this, "Account registration successful",
+                                    Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignUpActivity.this, "Account registration failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void reload() {}
+}
+
